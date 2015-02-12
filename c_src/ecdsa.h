fdaee846968d00c9e010eac85a25263f04d9030d
David Ellefsen <davidellefsen@gmail.com>
2015-02-12 10:01:41 +0200

    Add License information to the source files

--- c_src/ecdsa.h
+++ c_src/ecdsa.h
@@ -1,3 +1,23 @@
+/**
+ * A collection of ECDSA wrapper functions for Erlang
+ * Author: David Ellefsen <davidellefsen@gmail.com>
+ *
+ * Copyright 2015 David Ellefsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #ifndef ECDSA_H
 #define ECDSA_H
 
